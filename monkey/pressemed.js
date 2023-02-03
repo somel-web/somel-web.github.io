5b64bff6e9fd9b17d4dcf408cd3d46d3fa75f512
somel-web <76241341+somel-web@users.noreply.github.com>
2023-02-03 14:42:30 +0100

    a

--- monkey/pressemed.js
+++ monkey/pressemed.js
@@ -5,26 +5,48 @@
 // @match       https://www.lequotidiendumedecin.fr/*
 // @match 		https://www.legeneraliste.fr/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description 03/02/2023 05:30:19
 // ==/UserScript==
 
 if (document.getElementsByClassName("listing-body-article")[0]){
 	var t = document.getElementsByClassName("listing-body-article")[0].innerHTML;
-	var a = document.createElement('iframe');
-a.width = "600";
-a.height = "600";
+var link = document.createElement('link');
+    link.rel="stylesheet";
+    link.href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css";
 
-	a.style.position = "absolute";
-	a.style.top = "40px";
-	a.style.backgroundColor = "white";
-	a.style.zIndex = "100";
+document.body.appendChild(link);
+var script = document.createElement('script');
+script.src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" ;
+document.body.appendChild(script);
 
-document.querySelector('body').appendChild(a);
-	   var doc = a.contentWindow.document;
-   doc.open();
-   doc.write(t);
-   doc.close();
+var html =`
+<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+  voir article
+</button>
 
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">article</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div id="article">
+      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+    </div>
+  </div>
+</div>`;
+
+var div = document.createElement("div");
+div.classList.add("sticky-bottom");
+
+//div.style.position = "absolute";
+	div.style.top = "10px";
+	div.style.backgroundColor = "white";
+	div.style.zIndex = "10000";
+div.innerHTML = html;
+document.body.appendChild(div);
+document.getElementById("article").innerHTML = t;
 }
+
